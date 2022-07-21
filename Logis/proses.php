1eb8868b2bd4752b22cce868623c23ea1647dd72
rifkaatj <rifkaatmajati94@yahoo.com>
2022-07-21 17:19:26 +0800

    first commit

---
+++ Logis/proses.php
@@ -0,0 +1,14 @@
+<?php
+	$nama		= $_POST['inama'];
+	$tujuan     = $_POST['itujuan'];
+	$jarak		= $_POST['ijumlah'];
+	$biaya		= $_POST['otiket'];
+	$total		= $_POST['ototal'];
+	
+	echo "<head><title>baca</head></title>";
+	$fp = fopen("baca.txt", "a+");
+	fputs($fp, "$nama|$tujuan|$jarak|$biaya|$total\n");
+	fclose($fp);
+
+	echo "Terima kasih menggunakan jasa pengiriman JCC<br>";
+?>
